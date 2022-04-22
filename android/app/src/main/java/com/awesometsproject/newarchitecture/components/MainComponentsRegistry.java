563e9442c9d1eaddc302c361b1ed9a04a7c01e64
zheng.wang <zheng.Wang@mobiledrivetech.com>
2022-04-22 17:27:01 +0800

    add typescript

--- android/app/src/main/java/com/awesometsproject/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/awesometsproject/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.reactnativeunityapp.newarchitecture.components;
+package com.awesometsproject.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
